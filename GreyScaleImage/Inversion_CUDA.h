26de31e037e6c3b5451310ecd16d08bed743e20d
BNOK <52835202+BNOK@users.noreply.github.com>
2022-01-10 00:41:59 +0100

    Initial commit

---
+++ GreyScaleImage/Inversion_CUDA.h
@@ -0,0 +1,10 @@
+#pragma once
+#ifndef _Inversion_CUDA_
+#define _Inversion_CUDA_
+void Image_Inversion_CUDA(unsigned char* Input_Image, int Height, int Width, int Channels);
+#endif
+#ifndef _Grey_Scale_Image
+#define _Grey_Scale_Image 
+void Image_GreyScale_CUDA(unsigned char* Input_Image, int Height, int Width, int Channels);
+#endif // !1
+
