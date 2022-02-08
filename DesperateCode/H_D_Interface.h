69aff2f5cd4105ec19865aca6543328d39771056
BNOK <52835202+BNOK@users.noreply.github.com>
2022-02-08 03:18:45 +0100

    ,noice

--- DesperateCode/H_D_Interface.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#ifndef _CONVERT_TO_GREYSCALE
-#define _CONVERT_TO_GREYSCALE
-void ConvertToGreyScale(uchar* hout_Data, uchar* hin_Data, int rows, int cols, int channels);
-#endif // !_CONVERT_TO_GREYSCALE
-
